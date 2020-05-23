f0ed7ab0188583f8856f706867b50109c48cd38e
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-05-24 01:37:44 +0900

    Apply expo-location

--- App.tsx
+++ App.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Alert } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
 const App = () => {
-  return <Loading />;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const getLocation = async() => {
+    try {
+      // const response = await Location.requestPermissionsAsync();
+      // console.log(response);
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      // Send to API and get weather!
+      console.log(latitude, longitude);
+      setIsLoading(false);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  return (
+    isLoading ? <Loading /> : null
+  );
 }
 
 export default App;
