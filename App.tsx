0baab664ba2b3e0a797645b8bd17f8ecae22d8fa
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-06-20 21:00:00 +0900

    Displaying Temperature

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
 import * as Location from 'expo-location';
 import axios from 'axios';
 
@@ -8,12 +9,14 @@ const API_KEY = "718553c22b382b8afd28a6464c63deb3"
 
 const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [temp, setTemp] = useState<number>(0);
 
   const getWeather = async(latitude : number, longitude : number) => {
     const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     )
-    console.log(data);
+    setIsLoading(false);
+    setTemp(data.main.temp);
   }
 
   const getLocation = async() => {
@@ -23,7 +26,7 @@ const App = () => {
       const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
       // Send to API and get weather!
       console.log(latitude, longitude);
-      getWeather(latitude, longitude);
+      await getWeather(latitude, longitude);
       setIsLoading(false);
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
@@ -35,7 +38,7 @@ const App = () => {
   }, []);
 
   return (
-    isLoading ? <Loading /> : null
+    isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />
   );
 }
 
