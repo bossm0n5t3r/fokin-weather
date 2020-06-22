9e431814f00aab797b625b2ed9bb824865b9dc5d
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-06-22 23:49:51 +0900

    Work about weather condition

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
-import Weather from './Weather';
+import Weather, { CONDITION } from './Weather';
 import * as Location from 'expo-location';
 import axios from 'axios';
 
@@ -10,22 +10,22 @@ const API_KEY = "718553c22b382b8afd28a6464c63deb3"
 const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [temp, setTemp] = useState<number>(0);
+  const [condition, setCondition] = useState<string>(CONDITION.Clear);
 
   const getWeather = async(latitude : number, longitude : number) => {
-    const { data } = await axios.get(
+    const { data: {main: {temp}, weather} } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     )
+    console.log(weather[0].main);
     setIsLoading(false);
-    setTemp(data.main.temp);
+    setTemp(temp);
+    // TODO condition does not pass to Weather.tsx
+    setCondition(weather[0].main);
   }
 
   const getLocation = async() => {
     try {
-      // const response = await Location.requestPermissionsAsync();
-      // console.log(response);
       const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
-      // Send to API and get weather!
-      console.log(latitude, longitude);
       await getWeather(latitude, longitude);
       setIsLoading(false);
     } catch (error) {
@@ -38,7 +38,7 @@ const App = () => {
   }, []);
 
   return (
-    isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />
+    isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />
   );
 }
 
