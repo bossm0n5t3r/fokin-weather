7540c386072e229959afce25774bfdb2810de535
bossm0n5t3r <bossm0n5t3r@gmail.com>
2020-06-27 13:27:09 +0900

    Get weather condition

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
-import Weather from './Weather';
+import Weather, { CONDITION } from './Weather';
 import * as Location from 'expo-location';
 import axios from 'axios';
 
@@ -10,16 +10,14 @@ const API_KEY = "718553c22b382b8afd28a6464c63deb3"
 const App = () => {
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [temp, setTemp] = useState<number>(0);
-  const [condition, setCondition] = useState<string>('');
+  const [condition, setCondition] = useState<CONDITION>(CONDITION.Clear);
 
   const getWeather = async(latitude : number, longitude : number) => {
     const { data: { main: { temp }, weather } } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     )
-    console.log(weather[0].main);
     setIsLoading(false);
     setTemp(temp);
-    // TODO condition does not pass to Weather.tsx
     setCondition(weather[0].main);
   }
 
--- Weather.tsx
+++ Weather.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export const CONDITION = {
-  Thunderstorm : 'Thunderstorm',
-  Drizzle : 'Drizzle',
-  Rain : 'Rain',
-  Snow : 'Snow',
-  Atmosphere : 'Atmosphere',
-  Mist : 'Mist',
-  Smoke : 'Smoke',
-  Haze : 'Haze',
-  Dust : 'Dust',
-  Fog : 'Fog',
-  Sand : 'Sand',
-  Ash : 'Ash',
-  Squall : 'Squall',
-  Tornado : 'Tornado',
-  Clear : 'Clear',
-  Clouds : 'Clouds'
+export enum CONDITION {
+  Thunderstorm = 'Thunderstorm',
+  Drizzle = 'Drizzle',
+  Rain = 'Rain',
+  Snow = 'Snow',
+  Atmosphere = 'Atmosphere',
+  Mist = 'Mist',
+  Smoke = 'Smoke',
+  Haze = 'Haze',
+  Dust = 'Dust',
+  Fog = 'Fog',
+  Sand = 'Sand',
+  Ash = 'Ash',
+  Squall = 'Squall',
+  Tornado = 'Tornado',
+  Clear = 'Clear',
+  Clouds = 'Clouds'
 }
 
-const Weather = (
-  temp : any,
-  condition : string
-) => {
+interface IWeather {
+  temp: number,
+  condition: CONDITION
+}
+
+const Weather = ({ temp, condition } : IWeather) => {
   return (
     <View style={styles.container}>
-      <Text>{temp.temp}</Text>
+      <Text>{temp}</Text>
       <Text>{condition}</Text>
     </View>
   );
